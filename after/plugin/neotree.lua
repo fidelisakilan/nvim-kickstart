c2f53c2f4b7f041564fc538c54307b18be7d4376
Akilan <83576211+akilanfidelis@users.noreply.github.com>
2024-03-03 17:04:47 +0530

    removed neotree.lua

--- after/plugin/neotree.lua
+++
@@ -1,1 +0,0 @@
-vim.keymap.set('n', '<leader>v', vim.cmd.Neotree, { desc = 'Toggle Neotree' })
--- lua/me/lazy.lua
+++ lua/me/lazy.lua
@@ -226,13 +226,6 @@ require('lazy').setup({
     lazy = false,
   },
   {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-      "3rd/image.nvim",
-    }
-  }
+    "ThePrimeagen/vim-be-good",
+  },
 })
