082206902e330dbbc7ce4e675d06c0c5bd62bae7
Akilan <83576211+akilanfidelis@users.noreply.github.com>
2024-03-31 22:06:24 +0530

    changes

--- lua/me/lazy.lua
+++ lua/me/lazy.lua
@@ -157,9 +157,6 @@ require('lazy').setup({
     'catppuccin/nvim',
     priority = 1000,
     config = function()
-      require('catppuccin').setup({
-         transparent_background = true,
-      })
       vim.cmd.colorscheme 'catppuccin-mocha'
     end,
   },
